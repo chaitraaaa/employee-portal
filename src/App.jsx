8d1f1be7d917804e1e61012b6034c249920dfc80
chaitraaaa <103114017+chaitraaaa@users.noreply.github.com>
2026-08-01 12:38:35 +0530

    src app.jsx and main.jsx updating

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div>
+      <h1>Employee Portal</h1>
+      <h2>Welcome to the Employee Portal</h2>
+
+      <hr />
+
+      <button>Login</button>
+
+      <button>Dashboard</button>
+
+      <button>Profile</button>
+
+      <button>About</button>
+    </div>
+  )
+}
+
+export default App
