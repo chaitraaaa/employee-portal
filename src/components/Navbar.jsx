b0d21d466ec0dbbc7dce62eef5d8eb1cdefd5a88
chaitraaaa <103114017+chaitraaaa@users.noreply.github.com>
2026-08-08 21:55:34 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
+import React from "react";
+
 function Navbar() {
   return (
     <nav
       style={{
         backgroundColor: "#1976d2",
         color: "white",
-        padding: "15px"
+        padding: "15px",
       }}
     >
       <h2>Employee Portal</h2>
